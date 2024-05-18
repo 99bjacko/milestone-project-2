e077df1759b145b1aa77d103415853753201b769
Jack Applegate <japplegate2321@gmail.com>
2024-05-18 13:36:20 +0000

    Initialise questions array and add basic structure to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+const questionsArr = [
+    {
+        lyric: "Now I'm lookin' to the sky to save me, lookin' for a sign of life",
+        answers: [
+            "Learn to Fly - Foo Fighters",
+            "Many of Horror - Biffy Clyro",
+            "High and Dry - Radiohead",
+            "Thunder - BOYS LIKE GIRLS",
+        ],
+        correctAnswer: "Learn to Fly - Foo Fighters",
+
+    },
+];
+
+const descriptionContainer = document.getElementById("description-container");
+const gameContainer = document.getElementById("game-container");
+const resultsContainer = document.getElementById("results-container");
+
+function startGame() {
+
+}
+
+function randomiseQuestions() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function showResults() {
+
+}
+
+function restartGame() {
+    
+}
