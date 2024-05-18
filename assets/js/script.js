c2149950c9a2d9d0ba75cd371539f19a4ba2597d
Jack Applegate <japplegate2321@gmail.com>
2024-05-18 14:16:59 +0000

    Add IDs to elements themselves as well as their containers in index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ const descriptionContainer = document.getElementById("description-container");
 const gameContainer = document.getElementById("game-container");
 const resultsContainer = document.getElementById("results-container");
 const startBtn = document.getElementById("start-btn");
-const questionCount = document.getElementById("question-count");
+const questionNumber = document.getElementById("question-number");
 const questionText = document.getElementById("question-text");
 const answerBtns = document.getElementsByClassName("answer-btn");
 const nextQuestionButton = document.getElementById("next-question-btn");
@@ -25,6 +25,8 @@ const playerScore = document.getElementById("player-score");
 const resultsMessage = document.getElementById("results-message");
 const playAgainBtn = document.getElementById("play-again-btn");
 const homeBtn = document.getElementById("home-btn");
+
+startBtn.addEventListener("click", startGame);
 
 
 function startGame() {
